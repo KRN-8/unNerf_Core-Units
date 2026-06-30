4a04407abb1fa15dd5ca8496150ad405a8185927
KRN <67783847+KRN-8@users.noreply.github.com>
2026-07-01 00:56:59 +0200

    fork finished

--- scripts/unnerf.js
+++ scripts/unnerf.js
@@ -1,7 +1,7 @@
 Events.on(ContentInitEvent, () => {
-	Vars.content.blocks().each(b => b instanceof CoreBlock, b => {
-		b.unitType.weapons.each(w => {
+	Vars.content.blocks().each(b => b instanceof CoreBlock, u => {
+		u.unitType.weapons.each(w => {
 			w.bullet.buildingDamageMultiplier = 1;
-		})
-	})
+		});
+	});
 });
